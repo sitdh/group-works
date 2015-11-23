004052c0ac5005b4d747bd7dfbaa43338f902558
Sitdhibong Laokok <sitdhibong@gmail.com>
2015-11-23 10:40:12 +0700

    Refactoring comment retrieve

--- gws/gws.py
+++ gws/gws.py
@@ -24,40 +24,49 @@ def format_url(page_id = "", query = ""):
     credential = json_message()
     return "%s%s%s&access_token=%s" % (graph_url, page_id, query, credential['access_token'])
 
+def get_page_content(page_url, is_first_page=True):
+    page_request = urllib2.urlopen(age_url)
+    page_content_text = page_request.read()
+    page_content = json.loads(page_content_text)
+
+    return page_content['feed']['data'] if is_first_page else page['data']
+
+def get_comemnts(comments, datastore):
+
+    for comment in comments:
+
+        datastore.write("%s\n" % comment)
+
+        if comment['comment_count'] > 0:
+            get_comments(comment['comments']['data'], datastore)
+
 if '__main__' == __name__:
+
     pages = ['BulgeHd', 'Lickourdisplay', 'trasherbangkok', 'haruehun.airry', 'Toodsdiary', 'NeeNongNa', 'overtheeyebrow', 'cutegaystory', 'gaythaigointer', 'koorakgay', 'iamgaytherabbit', 'SingleGay.th', 'GthaiMovie', 'gaybottom']
 
     for page in pages:
         print "Page @%s" % page
 
-        p_url = format_url(page_id = page, query = '?fields=feed{comments{message,comment_count,comments{message}}}') 
+        page_url = format_url(page_id = page, query = '?fields=feed{comments{message,comment_count,comments{message}}}') 
 
         first_page = True
 
         for i in [1..10]:
 
-            page_request = urllib2.urlopen(p_url)
-            page_content_text = page_request.read()
-            page_content = json.loads(page_content_text)
+            page_content = get_page_content(page_url, first_page)
 
-            for message in page_content['feed']['data']:
+            first_page = False
+
+            for message in page_content:
 
                 if 'comments' in message:
 
                     with open('%s-comments.txt' % page, 'aw') as message_content:
-                        for msg in message['comments']['data']:
-                            message_content.write("%s\n" % msg['message'])
-
-                            if msg['comment_count'] > 0:
-
-                                for reply_comment in msg['comments']['data']:
-                                    message_content.write("%s\n" % reply_comment['message'])
-
+                        get_comment(message['comments']['data'], message_content)
                         message_content.close()
 
-                p_url = page_content['paging']['next']
-
-                time.sleep(30)
+            page_url = page_content['paging']['next']
+            time.sleep(30)
 
         time.sleep(30)
          
